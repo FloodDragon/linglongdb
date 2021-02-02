520280e590fe40026b68cea8b844b6a95228cf5d
liuj-ai <liuj-ai@glodon.com>
2021-02-02 16:20:22 +0800

    数据库核心代码努力迁移调整完成,准备二次开发中...

--- linglongdb-engine/src/test/java/com/glodon/linglong/engine/test/TestCRUD.java
+++ linglongdb-engine/src/test/java/com/glodon/linglong/engine/test/TestCRUD.java
@@ -20,7 +20,7 @@ public class TestCRUD {
                 .baseFilePath("C:\\Users\\liuj-ai\\Desktop\\数据库开发\\my-db\\data")
                 .minCacheSize(100_000_000)
                 .durabilityMode(DurabilityMode.SYNC);
-
+        long time = 0;
         //打开数据库
         Database db = Database.open(config);
         //开始事务
@@ -28,12 +28,22 @@ public class TestCRUD {
         //构建索引
         Index userIx = db.openIndex("userIndex");
         String msg = "正在进行数据库测试";
-        byte[] key;
-        userIx.insert(txn, key = String.valueOf(ThreadLocalRandom.current().nextInt()).getBytes(), msg.getBytes());
+
+        byte[] key = null;
+        for (int i = 1; i <= 10000000; i++) {
+            time = System.currentTimeMillis();
+            userIx.insert(txn, key = String.valueOf(ThreadLocalRandom.current().nextInt()).getBytes(), msg.getBytes());
+            System.out.println("写入数量: " + i + " 耗时: " + (System.currentTimeMillis() - time));
+            if (key != null) {
+                time = System.currentTimeMillis();
+                byte[] data = userIx.load(txn, key);
+                System.out.println("读取写入数据: " + new String(data == null ? new byte[0] : data) + " 耗时: " + (System.currentTimeMillis() - time));
+            }
+
+        }
         //事务提交 & 回滚
         txn.commit();//txn.reset();
-        byte[] data = userIx.load(txn, key);
-        System.out.println(new String(data == null ? new byte[0] : data));
+
         //索引关闭
         userIx.close();
         //关闭数据库
