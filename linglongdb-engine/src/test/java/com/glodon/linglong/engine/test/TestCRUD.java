486ec1cdab90fc3b5095ffb7b996140082fc93c6
liuj-ai <liuj-ai@glodon.com>
2021-02-06 14:50:11 +0800

    改进数据库单机读写测试,进行1E数量级测试.

--- linglongdb-engine/src/test/java/com/glodon/linglong/engine/test/TestCRUD.java
+++ linglongdb-engine/src/test/java/com/glodon/linglong/engine/test/TestCRUD.java
@@ -16,7 +16,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public final class TestCRUD {
 
     private static volatile long lastCount = 0;
-    private static final long testTotal = 10000000L;
+    private static final long testTotal = 20000000L;
     private static final AtomicLong counter = new AtomicLong(0);
     private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
     private static final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
@@ -34,6 +34,7 @@ public final class TestCRUD {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        long start = System.currentTimeMillis();
         System.out.println("数据库性能测试开始......");
         DatabaseConfig config = new DatabaseConfig()
                 .baseFilePath("C:\\Users\\liuj-ai\\Desktop\\数据库开发\\linglongdb\\data")
@@ -83,7 +84,7 @@ public final class TestCRUD {
         while (!executorService.awaitTermination(1000L, TimeUnit.MILLISECONDS)) ;
         //关闭数据库
         db.close();
-        System.out.println("数据库性能测试10s后即将关闭......");
+        System.out.println("数据库性能测试10s后即将关闭,总耗时" + ((System.currentTimeMillis() - start) / 1000) + "s ......");
         Thread.sleep(1000 * 10L);
         scheduledExecutorService.shutdown();
     }
