e81e0464a73f20c73f0bc19cf5c22e83f1b51dae
liuj-ai <liuj-ai@glodon.com>
2021-03-05 18:36:45 +0800

    正在开发协议模块...

--- linglongdb-protocol/src/main/java/com/linglong/protocol/Protocol.java
+++ linglongdb-protocol/src/main/java/com/linglong/protocol/Protocol.java
@@ -4,7 +4,17 @@ import com.linglong.rpc.serialization.msgpack.BeanMessage;
 
 /**
  * TODO 设计中...
+ *
  * @author Stereo
  */
 public abstract class Protocol implements BeanMessage {
+
+    //txId
+    //创建临时库
+    //拆卸临时库
+    //统计count
+    //打开Index/查找Index/exists Index/replace Index
+    //open tx/commit tx/rest tx
+    //kv store/kv exchange/kv insert/kv update/kv delete/kv load/kv replace
+    //sort
 }
