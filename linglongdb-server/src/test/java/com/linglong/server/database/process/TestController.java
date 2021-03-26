0c7b2e57df0ebb43f92eeb91b3ffb84ef64f7760
liuj-ai <liuj-ai@glodon.com>
2021-03-26 14:04:06 +0800

    数据库服务FIX数据库处理器BUG

--- linglongdb-server/src/test/java/com/linglong/server/database/process/TestController.java
+++ linglongdb-server/src/test/java/com/linglong/server/database/process/TestController.java
@@ -25,46 +25,50 @@ public class TestController {
 
         //执行数据库操作
         final String indexName = "index-test";
-        Response txnResponse = transactionProtocol.openTxn();
-        for (int i = 0; i < 10; i++) {
-            try {
-                KeyValueRequest insertKeyValue = new KeyValueRequest();
-                insertKeyValue.setId(new UUID(indexName).toString());
-                insertKeyValue.setIndexName(indexName);
-                insertKeyValue.setXid(txnResponse.getXid());
-                insertKeyValue.setKey(DatabaseProcessorTest.toBytes(i));
-                insertKeyValue.setValue(String.valueOf(i).getBytes());
-                KeyValueResponse keyValueResponse = keyValueProtocol.insert(insertKeyValue);
-                System.out.println("数据库测试 步骤0 写入" + i + (keyValueResponse.isSuccessful() ? "成功" : "失败"));
-                //Thread.sleep(1000L);
-            } catch (Exception ex) {
-                System.out.println("数据库测试 步骤0 写入" + i + "失败");
+
+        for (int j = 0; j < 10; j++) {
+            Response txnResponse = transactionProtocol.openTxn();
+            for (int i = 0; i < 10; i++) {
+                try {
+                    KeyValueRequest insertKeyValue = new KeyValueRequest();
+                    insertKeyValue.setId(new UUID(indexName).toString());
+                    insertKeyValue.setIndexName(indexName);
+                    insertKeyValue.setXid(txnResponse.getXid());
+                    insertKeyValue.setKey(DatabaseProcessorTest.toBytes(i));
+                    insertKeyValue.setValue(String.valueOf(i).getBytes());
+                    KeyValueResponse keyValueResponse = keyValueProtocol.insert(insertKeyValue);
+                    System.out.println("数据库测试 步骤0 写入" + i + (keyValueResponse.isSuccessful() ? "成功" : "失败"));
+                    //Thread.sleep(1000L);
+                } catch (Exception ex) {
+                    System.out.println("数据库测试 步骤0 写入" + i + "失败");
+                }
             }
+            System.out.println("数据库测试 步骤1 已写入完成.");
+
+            KeyLowHighRequest keyLowHighRequest = new KeyLowHighRequest();
+            keyLowHighRequest.setId(new UUID(indexName).toString());
+            keyLowHighRequest.setIndexName(indexName);
+            keyLowHighRequest.setXid(txnResponse.getXid());
+            CountResponse countResponse = indexProtocol.count(keyLowHighRequest);
+            System.out.println("数据库测试 步骤2 获取索引数据长度大小(" + countResponse.getCount() + ")");
+
+            IndexRequest indexRequest = new IndexRequest();
+            indexRequest.setId(new UUID(indexName).toString());
+            indexRequest.setIndexName(indexName);
+            IndexDeleteResponse deleteResponse = indexProtocol.delete(indexRequest);
+            System.out.println("数据库测试 步骤3 删除索引" + (deleteResponse.isDeleted() ? "成功" : "失败"));
+
+            countResponse = indexProtocol.count(keyLowHighRequest);
+            System.out.println("数据库测试 步骤4 获取索引数据长度大小(" + countResponse.getCount() + ")");
+
+            TxnRequest txnRequest = new TxnRequest();
+            txnRequest.setId(new UUID(indexName).toString());
+            txnRequest.setTxnId(txnResponse.getXid());
+            TxnCommitResponse txnCommitResponse = transactionProtocol.commitTxn(txnRequest);
+
+            System.out.println("数据库测试 步骤5 提交操作" + (txnCommitResponse.isCommited() ? "成功" : "失败"));
+
         }
-        System.out.println("数据库测试 步骤1 已写入完成.");
-
-//        KeyLowHighRequest keyLowHighRequest = new KeyLowHighRequest();
-//        keyLowHighRequest.setId(new UUID(indexName).toString());
-//        keyLowHighRequest.setIndexName(indexName);
-//        keyLowHighRequest.setXid(txnResponse.getXid());
-//        CountResponse countResponse = indexProtocol.count(keyLowHighRequest);
-//        System.out.println("数据库测试 步骤2 获取索引数据长度大小(" + countResponse.getCount() + ")");
-
-//        IndexRequest indexRequest = new IndexRequest();
-//        indexRequest.setId(new UUID(indexName).toString());
-//        indexRequest.setIndexName(indexName);
-//        IndexDeleteResponse deleteResponse = indexProtocol.delete(indexRequest);
-//        System.out.println("数据库测试 步骤3 删除索引" + (deleteResponse.isDeleted() ? "成功" : "失败"));
-
-//        countResponse = indexProtocol.count(keyLowHighRequest);
-//        System.out.println("数据库测试 步骤4 获取索引数据长度大小(" + countResponse.getCount() + ")");
-
-        TxnRequest txnRequest = new TxnRequest();
-        txnRequest.setId(new UUID(indexName).toString());
-        txnRequest.setTxnId(txnResponse.getXid());
-        TxnCommitResponse txnCommitResponse = transactionProtocol.commitTxn(txnRequest);
-
-        System.out.println("数据库测试 步骤5 提交操作" + (txnCommitResponse.isCommited() ? "成功" : "失败"));
 
         //客户端关闭
         clientProxy.stop();
